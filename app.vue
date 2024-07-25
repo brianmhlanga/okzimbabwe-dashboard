fef895d2cc0cd2865d0541935925081b42838c95
Brian Mhlanga <brianmhlanga9@gmail.com>
2024-07-25 06:18:54 +0200

    init

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <!-- <ConfirmDialog></ConfirmDialog>
+    <ConfirmDialog group="templating">
+        <template #message="slotProps">
+            <div class="flex p-4">
+                <i :class="slotProps.message.icon" style="font-size: 1.5rem"></i>
+                <p class="pl-2">{{slotProps.message.message}}</p>
+            </div>
+        </template>
+    </ConfirmDialog>
+    <ConfirmDialog group="positionDialog"></ConfirmDialog> -->
+    <Toast/>
+    <NuxtPage />
+  </main>
+</template>
+<style>
+
+</style>
+<script>
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primeicons/primeicons.css'
+import  "~/public/css/primeblocks.css"
+import "~/public/css/theme.min.css"
+// import "~/public/css/theme.min.css.map"
+// import "~/public/css/bootstrap.min.css.map"
+import "~/public/css/bootstrap.min.css"
+</script>
---
+++ layouts/auth.vue
@@ -0,0 +1,37 @@
+<template>
+    <main>
+      <link rel="stylesheet" href="css/bootstrap.min.css" />
+      <link rel="stylesheet" href="css/theme.min.css.map" />
+      <link rel="stylesheet" href="css/theme.min.css" />
+      <link rel="stylesheet" href="css/bootstrap.min.css.map" />
+      <link rel="stylesheet" href="vendors/css/vendors.min.css" />
+   <div class="layout-main-container">
+    <div class="layout-main">
+      <slot />
+    </div>
+  </div>
+  </main>
+</template>
+<script>
+export default {
+ head(){
+      return {
+          script: [
+              {
+                src: "js/bootstrap.bundle.min.js",
+              },
+              {
+                src: "js/vendors.min.js",
+              },
+              {
+                src: "js/common-init.min.js",
+              },
+              {
+                src: "js/theme-customizer-init.min.js",
+              }
+          ]
+      }
+  },
+}
+</script>
+
